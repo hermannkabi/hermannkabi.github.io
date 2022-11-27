2b6e045a8603a7ec24587c5b9d55895138d2b58d
Hermann <hermann.kaebi@gmail.com>
2022-11-27 11:27:44 +0200

    Added esimene advent

--- advent/esimene/index.js
+++ advent/esimene/index.js
@@ -1,42 +1,5 @@
-function onStart(){
-    setTimeout(function (){
-        $("#second").fadeIn(function (){
-            setTimeout(function (){
-                $("#third").fadeIn(function (){
-                    setTimeout(function (){
-                        $("#third, #second").fadeOut(function (){
-                            $("#first > h1").text("Noh, mis ootad???");
-                            setTimeout(function (){
-                                $("#second > h1").text("Tahad oma advendikinki. Palun väga");
-                                $("#second").fadeIn(function (){
-                                    $("#third > h1").text("HEAD ADVENTI!");
-                                    var interval = setInterval(function (){
-                                        $("#third").fadeToggle(400);
-                                    }, 400);
-                                    setTimeout(function (){
-                                        clearInterval(interval);
-
-                                        $("#first > h1").text("Omg, miks sa ikka veel siin oled?")
-                                        $("#third").remove();
-                                        $("#second").hide();
-
-                                        $("#second > h1").text("Uhhh, hea küll, ma annan sulle selle kingi, mis ma Emmale tegin");
-                                        setTimeout(()=>{
-                                            $("#second").fadeIn(function (){
-                                                setTimeout(()=>{
-                                                    window.location.href = "second-page.html";
-                                                }, 1000);
-                                            });
-                                        }, 2000);
-                                    }, 5000);
-                                });
-                            }, 1000);
-                        });
-                    }, 2000);
-                });
-            }, 2000);
-        });
-    }, 2000);
-}
-
-onStart();
+var elements = [];
+$("h1").hide();
+$("h1").each(function(index) {
+    $(this).delay(2000*index).fadeIn(1000);
+});
