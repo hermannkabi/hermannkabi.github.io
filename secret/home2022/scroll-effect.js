9c57bd20eeaafb66fa33bea8cfef7ce4b3d9e3f3
Hermann <hermann.kaebi@gmail.com>
2022-12-17 10:51:14 +0200

    Updated version of home page

---
+++ secret/home2022/scroll-effect.js
@@ -0,0 +1,14 @@
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+const hidden = document.querySelectorAll(".hidden");
+
+hidden.forEach((el) => {
+    observer.observe(el);
+});
