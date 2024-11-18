bf7fe20b9b715d2469bd84571ba03e0ee9d52dc6
Hermann <hermann.kaebi@gmail.com>
2024-11-18 20:33:10 +0200

    Esimene advent

--- advent/esimene/index.js
+++
@@ -1,5 +0,0 @@
-var elements = [];
-$("h1").hide();
-$("h1").each(function(index) {
-    $(this).delay(2000*index).fadeIn(1000);
-});
--- advent/viimane/index.js
+++
@@ -1,7 +0,0 @@
-const christmas = 1671832800;
-
-
-setInterval(() => {
-    var now = parseInt((new Date().getTime() / 1000).toFixed(0));
-    document.getElementById("seconds").innerText = christmas - now;
-}, 1000);
