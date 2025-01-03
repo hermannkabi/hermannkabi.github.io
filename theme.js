57d615639bfea3212b39de5c547fec46356bd9c6
hermannkabi <hermann.kaebi@gmail.com>
2025-01-03 13:54:59 +0200

    Back button

--- theme.js
+++ theme.js
@@ -4,6 +4,17 @@ themeBtn.className = 'theme-btn';
 themeBtn.innerHTML = document.documentElement.getAttribute('data-theme') == "dark" ? "<img src='/icons/summer.png'>" : "<img src='/icons/moon.png'>";
 document.body.prepend(themeBtn);
 
+const backBtn = document.createElement('div');
+backBtn.className = 'theme-btn back-btn';
+backBtn.innerHTML = '<img src="/icons/back.png">';
+
+
+if((window.location.pathname.replaceAll("/", ""))){
+    document.body.prepend(backBtn);
+}
+
+
+
 function checkTheme(){
     if(!window.localStorage.getItem("site-theme")){
         if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -35,3 +46,15 @@ themeBtn.addEventListener("click", function (){
     checkTheme();
 });
 
+backBtn.addEventListener("click", function (){
+    fallbackUrl = '/';
+    var prevPage = window.location.href;
+
+    window.history.go(-1);
+
+    setTimeout(function(){ 
+        if (window.location.href == prevPage) {
+            window.location.href = fallbackUrl; 
+        }
+    }, 100);
+});
